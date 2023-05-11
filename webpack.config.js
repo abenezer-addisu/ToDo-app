c1969552dabe413877ee9e612d21beabd3a2f4c9
abenezer-addisu <dasesatube@gmail.com>
2023-05-11 15:11:50 +0300

    Linters and Webpack Installed

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do List',
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
+};
